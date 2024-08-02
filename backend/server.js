6f9a332f0e793b3e4b03a46c22493a23cf834d04
johncarlou <john.c.mangab0@gmail.com>
2024-08-03 03:32:54 +0800

    update: fixing CORS issue

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(cors({
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(`${process.env.MONG_DBCONNECTION}`)
+mongoose.connect('mongodb+srv://john:john2024@cluster0.lt7s1va.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
   .then(() => console.log('Connected to MongoDB'))
   .catch((error) => console.error('Error connecting to MongoDB:', error));
 

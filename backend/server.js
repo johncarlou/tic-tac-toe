73891d78b4e536885080312527c971041a4dea45
John Carlou Mangabo <john.carlou.mangabo@accenture.com>
2024-08-03 23:11:16 +0800

    update: fixing CORS issue

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -6,9 +8,16 @@ const gameRoutes = require('./routes/game');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT'],
+  allowedHeaders: ['Content-Type'],
+  credentials: true, // Allow credentials
+}));
+
 app.use(express.json());
 
+PORT=5000
 // Connect to MongoDB
 mongoose.connect('mongodb+srv://john:john2024@cluster0.lt7s1va.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
   .then(() => console.log('Connected to MongoDB'))
@@ -22,6 +31,6 @@ app.get('/', (req, res) => {
 // Use game routes
 app.use('/games', gameRoutes);
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server is running on port ${process.env.PORT || 3000}`);
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server is running on port`, process.env.PORT || 5000);
 });
